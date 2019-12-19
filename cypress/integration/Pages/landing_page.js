0665770349c3b6cc07a0979c1dfd65a5f97bf5eb
Mubbrra.Inam <mubbrra@A002-00005.arbisoft.com>
2019-12-19 17:44:47 +0500

    delete test folder

---
+++ cypress/integration/Pages/landing_page.js
@@ -0,0 +1,5 @@
+import HelperFunctions from '../helpers/helper_functions'
+import LandingPage from '../Pages/landing_page.js'
+
+describe('landing page tests', function () {
+  const landingPage = new LandingPage()
