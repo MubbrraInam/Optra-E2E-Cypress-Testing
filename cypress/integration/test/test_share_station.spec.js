ea6bad46287e21005988ab14103f30e534de3500
Mubbrra.Inam <mubbrra@A002-00005.arbisoft.com>
2020-01-03 18:58:25 +0500

    share station

---
+++ cypress/integration/test/test_share_station.spec.js
@@ -0,0 +1,51 @@
+import login from '../pages/login_page.js'
+import landingPage from '../pages/landing_page.js'
+import sharestationpage from '../pages/search_share_station_page'
+
+
+// This is a basic structure for the tests and it needs some improvements.
+
+describe('Login tests', () => {
+
+    const fixtureImageFiles = ['test_1.jpeg', 'test_2.jpeg', 'test2.png']
+    const fileName = fixtureImageFiles[Math.floor(Math.random()*fixtureImageFiles.length)];
+    const uuidv4 = require('uuid/v4')
+    const randomString = uuidv4().substr(-6)
+    
+    beforeEach(()=> {
+        cy.visit("https://optra-test.auth.us-east-2.amazoncognito.com/login?response_type=code&client_id=5u5q09kfc7p2cnc70jk0k6n98e&redirect_uri=https://optra.demosites.cf/callback")
+        login.LoginForm(Cypress.env('USERNAME'), Cypress.env('PASSWORD'))
+        login.submitLoginForm()
+        
+    })
+  
+    it('lets the user login to the application', () => {
+        landingPage.openAccountManagement()
+        landingPage.getProfileButton().click()
+        landingPage.getSideMenu().should('contain', 'Profile').and('contain', 'Notifications').and('contain', 'Accounts Managed').and('contain', 'Send Feedback')
+    })
+
+    it('tests the search field validation ', () => {
+        //const lengthyString = uuidv4().substr(-31)
+        
+        landingPage.openSideMenu()
+        sharestationpage.getsharesation().within(() => {
+            cy.get('.search-form').click()
+            cy.get('.search-form').type('Volta HQ 05')
+            cy.get('.stationsList.filteredList')
+            cy.get('.p-0.arrow-btn.btn.btn-link').click()
+            
+            //Bookmar selected station
+            cy.get('.detail-holder button').click()
+       
+       
+       
+       
+       
+        })
+   
+  
+    })
+
+    
+})
